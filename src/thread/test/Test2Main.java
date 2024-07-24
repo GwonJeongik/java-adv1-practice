fb566d026eeb86dfb40fec4040917f920dea44ac
ik <jeongik11029@naver.com>
2024-07-24 22:26:36 +0900

    스레드 이름 "counter"로 변경

--- src/thread/test/Test2Main.java
+++ src/thread/test/Test2Main.java
@@ -15,8 +15,7 @@ import static util.MyLogger.*;
 public class Test2Main {
 
     public static void main(String[] args) {
-        CounterRunnable runnable = new CounterRunnable();
-        Thread thread = new Thread(runnable);
+        Thread thread = new Thread(new CounterRunnable(), "counter");
         thread.start();
     }
 
